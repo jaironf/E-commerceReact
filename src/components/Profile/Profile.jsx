a70a1f232460abce486adf17918994f9c4e0c324
jaironf <jaironf.94@gmail.com>
2024-05-29 13:44:51 +0200

    Añadido el estilo a Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { UserContext } from "../../context/UserContext/UserState";
 import { Spin } from "antd";
+import './Profile.scss'
 
 const Profile = () => {
   const { getLoggedUserInfo, user, token } = useContext(UserContext);
@@ -13,10 +14,15 @@ const Profile = () => {
     return <Spin size="large" />;
   }
   return (
-    <div>
-      <p> {user.name}</p>
-      <p> {user.email}</p>
+    <div className="profile-container">
+    <div className="profile-card">
+        <h2>Profile</h2>
+        <img className='profile-avatar' src="src/assets/AvatarPerfil.png" alt="" />
+        <p className="heading-profile">{user.name}</p>
+        <p>{user.email}</p>
+        <p>SkullGaming</p>
     </div>
+  </div>
   );
 };
 
