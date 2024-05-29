62701a509c6610d44a7e5938e77fbbf50d38b758
jaironf <jaironf.94@gmail.com>
2024-05-29 20:11:41 +0200

    Logout creado en UserState

--- src/context/UserContext/UserState.jsx
+++ src/context/UserContext/UserState.jsx
@@ -55,6 +55,22 @@ export const UserProvider = ({children}) =>{
         }
       };
 
+      const logout = async () =>{
+        let token = JSON.parse(localStorage.getItem('token'));
+        const res = await axios.delete(API_URL + '/logout', {
+          headers: {
+            Authorization: token,
+          }
+        });
+        dispatch({
+          type: 'LOGOUT',
+          payload: res.data,
+        });
+        if(res.data){
+          localStorage.removeItem('token')
+        }
+      };
+
 
     return (
         <UserContext.Provider
@@ -64,6 +80,7 @@ export const UserProvider = ({children}) =>{
             userRegistre,
             login,
             getLoggedUserInfo,
+            logout,
           }}
         >
           {children}
