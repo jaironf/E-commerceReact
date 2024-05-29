0e8ccde94bbebc9f2e3d322596a6cc52b16fd0fb
jaironf <jaironf.94@gmail.com>
2024-05-29 11:59:32 +0200

    Estilo a las cards en Products añadido

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 const Header = () => {
   return (
     <div className='navbar'>
-        <span className='navbar-brand'><img src="" alt="logo" className='logo-header'/></span>
+        <span className='navbar-brand'><img src="src/assets/LogoE-commerce.png" alt="logo" className='logo-header'/></span>
         <nav className='navbar-nav'>
             <span className='navbar-item'><Link to='/'>Home</Link></span>
             <span className='navbar-item'><Link to='/products'>Products</Link></span>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,8 +4,8 @@ import './Home.scss'
 const Home = () => {
   return (
     <div className='home-div'>
-       <span>LOGO DE LA EMPRESA</span>
-       {/* <button className='btn-home'><Link to='/form'>Welcome</Link></button> */}
+       <span><img src="src/assets/LogoE-commerce.png" alt="logo" /></span>
+       {/* <button className='btn-home'><Link to='/register'>Welcome</Link></button> */}
     </div>
   )
 }
